6292e1650953624999edffce003c87ec0659953d
Jaime Moncayo <jaimeandresmonc@gmail.com>
2019-03-15 00:06:51 -0500

    working interaction between window service and progressbar

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -11,7 +11,7 @@ import { WINDOW } from "../services/window.service";
 export class ProgressBarComponent implements OnInit {
 
   public navIsFixed: boolean = false;
-  
+
   constructor(
     @Inject(DOCUMENT) private document: Document,
     @Inject(WINDOW) private window: Window
@@ -24,8 +24,10 @@ export class ProgressBarComponent implements OnInit {
     let number = this.window.pageYOffset || this.document.documentElement.scrollTop || this.document.body.scrollTop || 0;
     if (number > 100) {
       this.navIsFixed = true;
+      console.log("hey hey hey")
     } else if (this.navIsFixed && number < 10) {
       this.navIsFixed = false;
+      console.log("hey")
     }
   }
 
