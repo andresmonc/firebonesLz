29f227d0b135ec58d44146c61f8cfbf1ab36b4c0
Jaime Moncayo <jaimeandresmonc@gmail.com>
2019-03-15 01:06:18 -0500

    working progress bar

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -11,7 +11,9 @@ import { WINDOW } from "../services/window.service";
 export class ProgressBarComponent implements OnInit {
 
   public navIsFixed: boolean = false;
-
+  public section1: boolean = true;
+  public section2: boolean = false;
+  public section3: boolean = false;
   constructor(
     @Inject(DOCUMENT) private document: Document,
     @Inject(WINDOW) private window: Window
@@ -22,12 +24,19 @@ export class ProgressBarComponent implements OnInit {
   @HostListener("window:scroll", [])
   onWindowScroll() {
     let number = this.window.pageYOffset || this.document.documentElement.scrollTop || this.document.body.scrollTop || 0;
-    if (number > 100) {
-      this.navIsFixed = true;
-      console.log("hey hey hey")
-    } else if (this.navIsFixed && number < 10) {
-      this.navIsFixed = false;
-      console.log("hey")
+    console.log(Math.round(number))
+    if (number < 1271) {
+      this.section1 = true;
+      this.section2 = false;
+      this.section3 = false;
+    } else if(number > 1271 && number < 2144) {
+      this.section1 = false;
+      this.section2 = true;
+      this.section3 = false;
+    } else if(number > 2144) {
+      this.section1 = false;
+      this.section2 = false;
+      this.section3 = true;
     }
   }
 
