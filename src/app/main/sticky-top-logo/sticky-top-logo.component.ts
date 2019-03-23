29b8eef8f1e2b563be8ad4ff05654744179ef182
Farhan-Islam <farhan.t.islam@gmail.com>
2019-03-23 01:48:04 -0700

    made sticky logo nav responsive

--- src/app/main/sticky-top-logo/sticky-top-logo.component.ts
+++ src/app/main/sticky-top-logo/sticky-top-logo.component.ts
@@ -25,14 +25,17 @@ export class StickyTopLogoComponent implements OnInit {
   @HostListener("window:scroll", [])
   onWindowScroll() {
 
+    console.log(this.document.body.clientWidth)
+
+    if(this.document.body.clientWidth < 855) {
+
       if (this.document.body.scrollTop > 110 || this.document.documentElement.scrollTop > 110) {
         this.bringTextCloser = true;
         this.document.getElementById("navbar").style.padding = "0px 10px";
-        this.document.getElementById("navbar").style.height = "265px";
-        this.document.getElementById("navbar").style.top = "-6%";
+        this.document.getElementById("navbar").style.height = "165px";
+        this.document.getElementById("navbar").style.top = "-2%";
         this.document.getElementById("navbar").style.backgroundColor = "black";
         this.document.getElementById("navbar").style.color = "#f9dfbe";
-        this.document.getElementById("logo").style.fontSize = "25px";
         this.document.getElementById("logo").style.height = "auto";
         this.document.getElementById("logo").style.transform =  'scale(' + (1 - 110 / 250) + ')'
         this.document.getElementById("logo").style.webkitTransition = "all .5s ease";
@@ -46,13 +49,43 @@ export class StickyTopLogoComponent implements OnInit {
         this.document.getElementById("navbar").style.top = "0";
         this.document.getElementById("navbar").style.backgroundColor = "#f1f1f100";
         this.document.getElementById("navbar").style.color = "black";
-        this.document.getElementById("logo").style.fontSize = "35px";
+        this.document.getElementById("logo").style.height = "auto";
+        this.document.getElementById("logo").style.width = "165px";
+        this.document.getElementById("logo").style.webkitTransition = "all .5s ease";
+        this.document.getElementById("logo").style.transition = "all .5s ease";
+        
+      }
+
+    } else {
+      if (this.document.body.scrollTop > 110 || this.document.documentElement.scrollTop > 110) {
+        this.bringTextCloser = true;
+        this.document.getElementById("navbar").style.padding = "0px 10px";
+        this.document.getElementById("navbar").style.height = "265px";
+        this.document.getElementById("navbar").style.top = "-6%";
+        this.document.getElementById("navbar").style.backgroundColor = "black";
+        this.document.getElementById("navbar").style.color = "#f9dfbe";
+        this.document.getElementById("logo").style.height = "auto";
+        this.document.getElementById("logo").style.transform =  'scale(' + (1 - 110 / 250) + ')'
+        this.document.getElementById("logo").style.webkitTransition = "all .5s ease";
+        this.document.getElementById("logo").style.transition = "all .5s ease";
+        
+      } else {
+        this.bringTextCloser = false;
+        this.document.getElementById("navbar").style.height = "auto";
+        this.document.getElementById("logo").style.transform =  'scale(' + (1 - this.document.documentElement.scrollTop / 250) + ')'
+        this.document.getElementById("navbar").style.padding = "10px 10px";
+        this.document.getElementById("navbar").style.top = "0";
+        this.document.getElementById("navbar").style.backgroundColor = "#f1f1f100";
+        this.document.getElementById("navbar").style.color = "black";
         this.document.getElementById("logo").style.height = "auto";
         this.document.getElementById("logo").style.width = "auto";
         this.document.getElementById("logo").style.webkitTransition = "all .5s ease";
         this.document.getElementById("logo").style.transition = "all .5s ease";
         
       }
+    }
+
+
 
     }
 
