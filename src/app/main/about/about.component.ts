8adb9bc647f71bc39f2a4eab559fc2cea458d364
Farhan-Islam <farhan.t.islam@gmail.com>
2019-03-24 02:10:06 -0700

    added arrow fade on scroll

--- src/app/main/about/about.component.ts
+++ src/app/main/about/about.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Inject } from "@angular/core";
+import { DOCUMENT } from "@angular/platform-browser";
+import { WINDOW } from "../../services/window.service";
 
 @Component({
   selector: 'app-about',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  private arrowFade: boolean = false;
+
+  private arrowFadepoint: number;
+
+  constructor(    
+  @Inject(DOCUMENT) private document: Document,
+  @Inject(WINDOW) private window: Window
+  ) { }
 
   ngOnInit() {
+    this.arrowFadepoint = 20
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+  
+    let number = this.window.pageYOffset || this.document.documentElement.offsetTop || this.document.body.scrollTop || 0;
+    console.log("scroll numb",number);
+    if (number >=  this.arrowFadepoint ) {
+      console.log("arrow fade true")
+      this.arrowFade = true;
+
+    }
+    else{
+      console.log("arrow fade false")
+      this.arrowFade = false;
+    }
+
   }
 
 }
