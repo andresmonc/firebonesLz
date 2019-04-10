fa7c96966c32e87d48e87c5a014fba491f80f661
Farhan-Islam <farhan.t.islam@gmail.com>
2019-04-09 21:01:04 -0700

    fied iphone 5 responsive issue

--- src/app/main/sticky-top-logo/sticky-top-logo.component.ts
+++ src/app/main/sticky-top-logo/sticky-top-logo.component.ts
@@ -25,7 +25,39 @@ export class StickyTopLogoComponent implements OnInit {
   @HostListener("window:scroll", [])
   onWindowScroll() {
 
-    if(this.document.body.clientWidth < 900) {
+    if(this.document.body.clientWidth < 399) {
+
+      if (this.document.body.scrollTop > 110 || this.document.documentElement.scrollTop > 110) {
+        this.bringTextCloser = true;
+        this.document.getElementById("navbar").style.padding = "0px 10px";
+        this.document.getElementById("navbar").style.height = "130px";
+        this.document.getElementById("navbar").style.top = "-2%";
+        this.document.getElementById("navbar").style.backgroundColor = "black";
+        this.document.getElementById("navbar").style.color = "#f9dfbe";
+        this.document.getElementById("logo").style.height = "auto";
+        this.document.getElementById("logo").style.transform =  'scale(' + (1 - 110 / 250) + ')'
+        this.document.getElementById("logo").style.webkitTransition = "all .5s ease";
+        this.document.getElementById("logo").style.transition = "all .5s ease";
+        this.document.getElementsByClassName("")
+        
+      } else {
+        this.bringTextCloser = false;
+        this.document.getElementById("navbar").style.height = "auto";
+        this.document.getElementById("logo").style.transform =  'scale(' + (1 - this.document.documentElement.scrollTop / 250) + ')'
+        this.document.getElementById("navbar").style.padding = "10px 10px";
+        this.document.getElementById("navbar").style.top = "0";
+        this.document.getElementById("navbar").style.backgroundColor = "#f1f1f100";
+        this.document.getElementById("navbar").style.color = "black";
+        this.document.getElementById("logo").style.height = "auto";
+        this.document.getElementById("logo").style.width = "120px";
+        this.document.getElementById("logo").style.webkitTransition = "all .5s ease";
+        this.document.getElementById("logo").style.transition = "all .5s ease";
+        
+      }
+
+    } 
+
+    else if(this.document.body.clientWidth < 900 && this.document.body.clientWidth > 400) {
 
       if (this.document.body.scrollTop > 110 || this.document.documentElement.scrollTop > 110) {
         this.bringTextCloser = true;
@@ -54,7 +86,11 @@ export class StickyTopLogoComponent implements OnInit {
         
       }
 
-    } else {
+    } 
+    
+    
+    
+    else {
       if (this.document.body.scrollTop > 110 || this.document.documentElement.scrollTop > 110) {
         this.bringTextCloser = true;
         this.document.getElementById("navbar").style.padding = "0px 10px";
