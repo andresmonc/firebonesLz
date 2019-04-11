ff185eee0f25c52262d52a691dd96104fa013b3b
Jaime Moncayo <jaimeandresmonc@gmail.com>
2019-04-10 20:53:21 -0500

    progress bar tracks page status; needs clickable nav

--- src/app/nav/progress-bar/progress-bar.component.ts
+++ src/app/nav/progress-bar/progress-bar.component.ts
@@ -10,21 +10,21 @@ import { WINDOW } from "../../services/window.service";
 })
 export class ProgressBarComponent implements OnInit {
 
-  private section1: boolean = true;
-  private section2: boolean = false;
-  private section3: boolean = false;
-  private section4: boolean = false;
-  private section5: boolean = false;
+  public section1: boolean = true;
+  public section2: boolean = false;
+  public section3: boolean = false;
+  public section4: boolean = false;
+  public section5: boolean = false;
+  public section6: boolean = false;
 
 
   private aboutOffset: number;
+  private whatIsOffset: number;
   private videoOffset: number;
   private audioOffset: number;
   private contactOffset: number;
   private subscribeOffset: number;
 
-  public loadBar: boolean = false;
-  private triggerBeforeElem: number = 0;
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
@@ -32,55 +32,48 @@ export class ProgressBarComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.videoOffset = (this.document.getElementById('videoSection').offsetTop) - this.triggerBeforeElem;
-    this.audioOffset = (this.document.getElementById('audioSection').offsetTop) - this.triggerBeforeElem;
-    this.aboutOffset = (this.document.getElementById('about').offsetTop) - this.triggerBeforeElem;
-    this.subscribeOffset = (this.document.getElementById('subscribe').offsetTop) - 500;
-    this.contactOffset = (this.document.getElementById('contact').offsetTop) - 500;
+    this.aboutOffset = (this.document.getElementById('about').offsetTop);
+    this.whatIsOffset = (this.document.getElementById('whatIsFireBones').offsetTop);
+    this.audioOffset = (this.document.getElementById('audioSection').offsetTop);
+    this.videoOffset = (this.document.getElementById('videoSection').offsetTop + 100);
+    this.subscribeOffset = (this.document.getElementById('subscribe').offsetTop);
+    this.contactOffset = (this.document.getElementById('contact').offsetTop);
+    console.log(this.aboutOffset);
+    console.log(this.whatIsOffset);
+    console.log(this.audioOffset);
+    console.log(this.videoOffset);
+    console.log(this.subscribeOffset);
+    console.log(this.contactOffset);
   }
 
   @HostListener("window:scroll", [])
   onWindowScroll() {
+
     console.log(this.subscribeOffset)
     let number = this.window.pageYOffset || this.document.documentElement.offsetTop || this.document.body.scrollTop || 0;
-    console.log("current scroll Pos: "+ number)
-    if (number >= this.aboutOffset && number <= this.videoOffset) {
+    console.log("current scroll Pos: " + number)
+    if (number <= this.aboutOffset) {
       this.section1 = true;
       this.section2 = false;
-      this.section3 = false;
-      this.section4 = false;
-      this.section5 = false;
-    } else if (number >= this.videoOffset && number <= this.audioOffset) {
+    } else if (number <= this.whatIsOffset) {
       this.section1 = false;
       this.section2 = true;
       this.section3 = false;
-      this.section4 = false;
-      this.section5 = false;
-      this.loadBar = true;
-    } else if (number >= this.audioOffset && number <= this.subscribeOffset) {
-      this.section1 = false;
+    } else if ( number <= this.audioOffset) {
       this.section2 = false;
       this.section3 = true;
       this.section4 = false;
-      this.section5 = false;
-      this.loadBar = true;
-    }
-    else if (number >= this.subscribeOffset && number <= this.contactOffset) {
-      this.section1 = false;
-      this.section2 = false;
+    } else if ( number <= this.videoOffset) {
       this.section3 = false;
       this.section4 = true;
       this.section5 = false;
-      this.loadBar = true;
-    }
-    else if (number >= this.contactOffset) {
-      this.section1 = false;
-      this.section2 = false;
-      this.section3 = false;
+    } else if ( number <= this.subscribeOffset) {
       this.section4 = false;
       this.section5 = true;
-      this.loadBar = true;
+      this.section6 = false;
+    } else if (number > this.subscribeOffset) {
+      this.section5 = false;
+      this.section6 = true;
     }
   }
-
 }
